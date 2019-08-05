8a39551a515e97ffb3888325732d18bd167dff42
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-05 17:51:07 +0800

    implement api

---
+++ impl/src/main/java/com/magictown/freeworld/peterpan/service/api/impl/ImplApplication.java
@@ -0,0 +1,13 @@
+package com.magictown.freeworld.peterpan.service.api.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplApplication.class, args);
+	}
+
+}
