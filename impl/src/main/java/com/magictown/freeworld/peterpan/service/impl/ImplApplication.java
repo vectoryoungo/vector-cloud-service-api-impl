49006de97672c9da3fe8c97f644d79daf946c44d
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-12 15:52:39 +0800

    add hystrix monitor

--- impl/src/main/java/com/magictown/freeworld/peterpan/service/impl/ImplApplication.java
+++ impl/src/main/java/com/magictown/freeworld/peterpan/service/impl/ImplApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class ImplApplication {
 
 	public static void main(String[] args) {
